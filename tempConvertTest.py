96488910dd60acf322747303e4e7042353e8c51e
tkpb <thiagokalleb@gmail.com>
2015-09-11 20:33:06 -0300

    Turn into Python 3 code.

--- tempConvertTest.py
+++ tempConvertTest.py
@@ -3,8 +3,7 @@ from suds.client import Client
 wsdlFile = 'http://www.w3schools.com/webservices/tempconvert.asmx?WSDL'
 
 client = Client(wsdlFile)
-#print client
 tF = '212'
 tC = '0'
-print '%s F = %s C\n' % (tF, client.service.FahrenheitToCelsius(tF))
-print '%s C = %s F' % (tC, client.service.CelsiusToFahrenheit(tC))
+print('%s F = %s C\n' % (tF, client.service.FahrenheitToCelsius(tF)))
+print('%s C = %s F' % (tC, client.service.CelsiusToFahrenheit(tC)))
