697e93ce18416ac570cb2fd2abcdf5f3b9744aff
tkpb <thiagokalleb@gmail.com>
2015-09-11 22:43:56 -0300

    Remove useless comments.

--- tempConvertTest.py
+++ tempConvertTest.py
@@ -5,5 +5,5 @@ wsdlFile = 'http://www.w3schools.com/webservices/tempconvert.asmx?WSDL'
 client = Client(wsdlFile)
 tF = '212'
 tC = '0'
-print('%s F = %s C\n' % (tF, client.service.FahrenheitToCelsius(tF)))
-print('%s C = %s F' % (tC, client.service.CelsiusToFahrenheit(tC)))
+print '%s F = %s C\n' % (tF, client.service.FahrenheitToCelsius(tF))
+print '%s C = %s F' % (tC, client.service.CelsiusToFahrenheit(tC))
