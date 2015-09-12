5023cb9cfc928fd8d2d7b575e97ea7e29c346c1c
tkpb <thiagokalleb@gmail.com>
2015-09-11 22:50:01 -0300

    Define methods that consume the service.

--- tempConvertTest.py
+++ tempConvertTest.py
@@ -1,9 +1,12 @@
 from suds.client import Client
 
 wsdlFile = 'http://www.w3schools.com/webservices/tempconvert.asmx?WSDL'
-
 client = Client(wsdlFile)
-tF = '212'
-tC = '0'
-print '%s F = %s C\n' % (tF, client.service.FahrenheitToCelsius(tF))
-print '%s C = %s F' % (tC, client.service.CelsiusToFahrenheit(tC))
+
+
+def fromC(temperature):
+    return client.service.CelsiusToFahrenheit(temperature)
+
+
+def fromF(temperature):
+    return client.service.FahrenheitToCelsius(temperature)
