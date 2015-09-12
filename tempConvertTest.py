fb631d5e8f2ec9b0c2bf3743b21772c4bc3a6c5f
tkpb <thiagokalleb@gmail.com>
2015-09-11 23:05:55 -0300

    Make user-interactive client.

--- tempConvertTest.py
+++ tempConvertTest.py
@@ -2,6 +2,8 @@ from suds.client import Client
 
 wsdlFile = 'http://www.w3schools.com/webservices/tempconvert.asmx?WSDL'
 client = Client(wsdlFile)
+userImput = ''
+tempChoice = 0
 
 
 def fromC(temperature):
@@ -10,3 +12,20 @@ def fromC(temperature):
 
 def fromF(temperature):
     return client.service.FahrenheitToCelsius(temperature)
+
+
+def setup():
+    global tempChoice
+    while tempChoice != 1 or 2:
+        tempChoice = input("Escolha a operacao: (1) C para F (2) F para C")
+
+
+setup()
+while userImput != 'exit':
+    userImput = input("Temperatura para converter: ")
+    if tempChoice == 1:
+        fromC(userImput)
+    elif tempChoice == 2:
+        fromF(userImput)
+    else:
+        setup()
